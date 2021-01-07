657fd3fd636ea83f93bd7f29232414f7eb243491
Anjani Kodali <anjani.katabathuni@gmail.com>
2021-01-07 13:43:40 -0800

    initial feeds service commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=anjvendev1
+export POSTGRES_PASSWORD=anjvendev1
+export POSTGRES_HOST=anjvendev1.c7nucnuz9p7d.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=anjvendev1
+export AWS_BUCKET=anj-ven-dev
+export AWS_REGION=us-west-2
+export AWS_PROFILE=AnjaniDev
+export JWT_SECRET=hello
+export URL=http://localhost:8100
